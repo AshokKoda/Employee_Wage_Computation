2c3b686a220d42869b6edc37d5d9c8fb581a49d7
Ashok Koda <a.koda59@gmail.com>
2021-12-17 17:46:11 +0530

    Calculate daily employee wage

--- src/employeewage/EmployeeWageComputation.java
+++ src/employeewage/EmployeeWageComputation.java
@@ -3,6 +3,9 @@ package employeewage;
 public class EmployeeWageComputation {
 	
 	public static int fulltime = 1;
+	public static int RatePerHour = 20;
+	public static int empHrs = 0;
+	public static int empWage = 0;
 	
 	/*
 	 * UC1: Employee Present or Absent
@@ -18,11 +21,28 @@ public class EmployeeWageComputation {
 			System.out.println("Employee is Absent");
 		}
 	}
+	
+	/*
+	 * Calculate daily employee wage
+	 */
+	public static void dailyEmployeeWage() {
+		
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		
+		if(empCheck == fulltime)
+			empHrs = 8;
+		else
+			empHrs = 0;
+		
+		empWage = empHrs * RatePerHour;
+		System.out.println("Employee Wage is : " + empWage);
+	}
 
 	public static void main(String[] args) {
 		System.out.println("------Welcome To Employee Wage Computation------");
 		
 		employeeAttendence();
+		dailyEmployeeWage();
 	}
 
 }
