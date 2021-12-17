c59498e595e134d6ee8661f13ee56ec22c7d5361
Ashok Koda <a.koda59@gmail.com>
2021-12-17 18:18:57 +0530

    Employee part time & wage

--- src/employeewage/EmployeeWageComputation.java
+++ src/employeewage/EmployeeWageComputation.java
@@ -1,48 +1,68 @@
 package employeewage;
 
 public class EmployeeWageComputation {
-	
+
 	public static int fulltime = 1;
+	public static int partTime = 2;
 	public static int RatePerHour = 20;
 	public static int empHrs = 0;
 	public static int empWage = 0;
-	
+
 	/*
 	 * UC1: Employee Present or Absent
 	 */
 	public static void employeeAttendence() {
-		
+
 		double random = Math.floor(Math.random() * 10) % 2;
-		
-		if(random == fulltime) 
-		{
+
+		if (random == fulltime) {
 			System.out.println("Employee is Present");
-		}else {
+		} else {
 			System.out.println("Employee is Absent");
 		}
 	}
-	
+
 	/*
 	 * Calculate daily employee wage
 	 */
 	public static void dailyEmployeeWage() {
-		
+
 		double empCheck = Math.floor(Math.random() * 10) % 2;
-		
-		if(empCheck == fulltime)
+
+		if (empCheck == fulltime)
 			empHrs = 8;
 		else
 			empHrs = 0;
-		
+
 		empWage = empHrs * RatePerHour;
 		System.out.println("Employee Wage is : " + empWage);
 	}
 
+	/*
+	 * Employee part time & wage
+	 */
+	public static void employeePartTime() {
+
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+
+		empWage = empHrs * RatePerHour;
+		
+		if (empCheck == partTime) {
+			empHrs = 8;
+			System.out.println("Part Time Wage: " + empWage);
+		}else {
+			empHrs = 0;
+			System.out.println("Employee is full time");
+		}
+			
+	}
+
 	public static void main(String[] args) {
 		System.out.println("------Welcome To Employee Wage Computation------");
-		
+
 		employeeAttendence();
 		dailyEmployeeWage();
+		employeePartTime();
 	}
 
 }
