0628ca14845242af694eef88b01058922b0cb9f1
Ashok Koda <a.koda59@gmail.com>
2021-12-17 16:48:17 +0530

    UC1: Employee present or absent

--- src/employeewage/EmployeeWageComputation.java
+++ src/employeewage/EmployeeWageComputation.java
@@ -4,6 +4,19 @@ public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
 		System.out.println("------Welcome To Employee Wage Computation------");
+		
+		/*
+		 * UC1: Employee Present or Absent
+		 */
+		int fulltime = 1;
+		double random = Math.floor(Math.random() * 10) % 2;
+		
+		if(random == fulltime) 
+		{
+			System.out.println("Employee is Present");
+		}else {
+			System.out.println("Employee is Absent");
+		}
 
 	}
 
